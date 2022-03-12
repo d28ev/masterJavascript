eae9b6ce504133deecd79e818b8eb6715657aaac
Devendra Yadav <kumaryadav28dev@gmail.com>
2022-03-12 08:52:03 +0530

    Added Promises

---
+++ Javascript Interviews/fetch.js
@@ -0,0 +1,35 @@
+function updateBooks(books){
+    var output = '';
+    books.forEach(book => {
+        output += `<li> testing boy Name : ${book.name}, Author : ${book.author} </li>`;
+    })
+
+    document.getElementById('books').innerHTML = output;
+}
+
+function error(error){
+    console.log('error message : '+ error);
+}
+
+function fetchData(){
+    alert('testing');
+
+    //Using promise
+   /* var promise = fetch('https://www.abibliadigital.com.br/api/books');
+    promise.then(res => {
+        return res.json();
+    })
+    .then(updateBooks)
+    .catch(error); */
+
+    //using async await 
+    fetchDataAsync();
+    
+}
+
+async function fetchDataAsync(){
+    alert('fadjan');
+    var promise = await fetch('https://www.abibliadigital.com.br/api/books');
+    var data = await promise.json();
+    updateBooks(data);
+}
